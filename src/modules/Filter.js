b36d91a8340e1466f7a2774262420ac6ee69d604
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-30 19:26:19 +0300

    nav

--- src/modules/Filter.js
+++ src/modules/Filter.js
@@ -1,15 +1,20 @@
 import { module } from "modujs";
 
+
+
 export default class extends module {
   constructor(m) {
     super(m);
   }
 
   init() {
-    window.addEventListener("load", ()=>{
-      console.log('true')
-      FsAttributes.cmsfilter.destroy()
-      FsAttributes.cmsfilter.init()
-    })
-    }
+
+    console.log("true");
+    setTimeout(()=>{
+      FsAttributes.cmsfilter.destroy();
+      FsAttributes.cmsfilter.init();
+    }, 50)
+   
+
+  }  
 }
