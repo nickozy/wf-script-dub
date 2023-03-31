e7733cc9c6440c76619816832d840d3718c75444
nickozy <104327959+nickozy@users.noreply.github.com>
2023-03-31 10:43:57 +0300

    scrollbar

--- src/modules/Scrollbar.js
+++ src/modules/Scrollbar.js
@@ -23,10 +23,8 @@ export default class extends module {
     updateScroll(){
         const container = document.querySelector(".store-locator__collection")
         container.scrollTop = 0;
-        if(window.innerWidth < 767){
         const elem = document.querySelector(".store-locator__filter")
         elem.classList.remove("is-open")
-    }
     }
     openScroll(){
         const elem = document.querySelector(".store-locator__filter")
