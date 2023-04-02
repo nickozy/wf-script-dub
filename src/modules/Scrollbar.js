5db6660a1b8f040dd93b91063ece086814c27224
nickozy <104327959+nickozy@users.noreply.github.com>
2023-04-02 17:26:44 +0300

    scrollbar

--- src/modules/Scrollbar.js
+++ src/modules/Scrollbar.js
@@ -21,7 +21,7 @@ export default class extends module {
         });
     }
     updateScroll(){
-        const container = this.el
+        const container = this.scrollbar; 
         container.scrollTop = 0;
         const elem = document.querySelector(".store-locator__filter")
         elem.classList.remove("is-open")
