c9a9ba816ba007071f212efeb9f92a6114dab8f5
nickozy <104327959+nickozy@users.noreply.github.com>
2023-04-03 10:24:10 +0300

    scrollbar

---
+++ src/modules/Video.js
@@ -0,0 +1,16 @@
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+        this.events = {
+            click: {
+              click: 'toggle',
+            }
+          }
+    }
+    toggle(){
+        let videoEl = document.getElementsByTagName('video')[0];
+        videoEl.muted = true;
+}
+}
