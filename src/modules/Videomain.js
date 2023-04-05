26d084d31db5488d375be1da6d0d93eb5abc5a49
nickozy <104327959+nickozy@users.noreply.github.com>
2023-04-05 20:57:05 +0300

    video src

---
+++ src/modules/Videomain.js
@@ -0,0 +1,17 @@
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+    }
+    init(){
+        const el = this.el
+        if(window.innerWidth > 767){
+        el.setAttribute("src", "https://data.d.starterapp.co/api/v1/buckets/breadhead/objects/download?preview=true&prefix=ZHViX2NhbXBhaWduLWRlc2t0b3AubXA0&version_id=null")
+    }
+    else{
+        el.setAttribute("src", "https://data.d.starterapp.co/api/v1/buckets/breadhead/objects/download?preview=true&prefix=Tk8gTE9HT19EVUJfQ2FtcGFpZ25fdGVhc2VyX21vYmlsZV8zMDAwLm1wNA==&version_id=null")
+
+    }
+}
+}  
