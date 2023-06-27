0e794a04ea81f2aaa292204f09eea16f87f14f4b
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-28 02:28:26 +0300

    lookback main

--- src/modules/GradientMorph.js
+++ src/modules/GradientMorph.js
@@ -1,13 +1,12 @@
 import { GradientMorph } from "@leon/gradient-morph";
 
 
-
 export default class extends module {
   constructor(m) {
     super(m);
   }
 
   init() {
-   GradientMorph()
+    GradientMorph.init()
 }
 }
